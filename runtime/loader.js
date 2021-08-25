7f8012a48c3b05d360139677dec8117bf269d56e
AnnaGo-cf <anna.goncharova@confirmit.com>
2021-08-25 12:04:17 +0300

    removed unnecessary console.log

--- runtime/loader.js
+++ runtime/loader.js
@@ -2,8 +2,6 @@
 	Confirmit.pageView.registerCustomQuestion(
 		"83f8d1ad-7949-4c53-9cda-aa424ece246b",
 		function (question, customQuestionSettings, questionViewSettings) {
-			console.log("in register");
-
 			new customQuestionsLibrary.SliderOpenQuestionView(question, questionViewSettings, customQuestionSettings);
 		}
 	);
